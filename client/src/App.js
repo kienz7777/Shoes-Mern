eae14322550aabdc5fee687d7e545d620c5c8e0d
kienz7777 <kienvu7777@gmail.com>
2021-01-28 22:09:18 +0700

    set up project

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
+// npm i --save axios js-cookie jsonwebtoken react-facebook-login react-google-login react-router-dom react-toastify
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+import Home from './features/Home';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return(
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" exact component={Home}/>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/features/Home/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home(props) {
+    return (
+        <div>
+            hêlo
+        </div>
+    );
+}
+
+export default Home;
