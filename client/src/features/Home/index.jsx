3c6e27212aaca8c5c146a3c69e86bae94db608ab
kienz7777 <kienvu7777@gmail.com>
2021-02-10 19:49:42 +0700

    css

--- client/src/features/Home/index.jsx
+++ client/src/features/Home/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Banner from '../../components/Banner';
 import Header from '../../components/Header';
-import './home.css';
+import './home.scss';
 
 function Home(props) {
     return (
