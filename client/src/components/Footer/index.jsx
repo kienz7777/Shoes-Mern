426aa80014b9483964d022a7ec544ee452286cd9
kienz7777 <kienvu7777@gmail.com>
2021-02-20 22:33:49 +0700

    footer p1

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -1,11 +1,42 @@
 import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
+
+import './footer.scss';
 
 
 function Footer(props) {
     return (
-        <div>
-            
-        </div>
+        <footer>
+            <Container>
+                <div className="newletter">
+                    <div className="newletter-title">
+                        <h3>Subscribe Newletter.</h3>
+                        <label>Get e-mail updates about our latest shop and special offers.</label>
+                    </div>
+                    <div className="newletter-content">
+                        <form className="frm-subscribe">
+                            <input type="text" placeholder="Enter you email address here..." id="subscribe-input"></input>
+                            <a className="btn-default btn-subscribe" >
+                                Subcribe
+                            </a>
+                        </form>
+                        <div className="warning"> Error : Please enter valid email address.</div>
+                    </div>
+                </div>
+
+                <div className="follow">
+                    <ul className="link-follow">
+                        <li><a><i className="fa fa-twitter"></i></a></li>
+                        <li><a><i className="fa fa-google-plus"></i></a></li>
+                        <li><a><i className="fa fa-facebook"></i></a></li>
+                        <li><a><i className="fa fa-youtube"></i></a></li>
+                        <li><a><i className="fa fa-instagram"></i></a></li>
+                    </ul>
+                </div>
+
+                
+            </Container>
+        </footer>
     );
 }
 
