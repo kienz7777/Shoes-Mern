125cf29875c9487872095b169a9311cb5acd076f
kienz7777 <kienvu7777@gmail.com>
2021-02-24 20:27:53 +0700

    connect mongooDB atlas

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const connectDB = require('./config/connectDB')
 require('dotenv').config()
 
 const app = express();
@@ -9,6 +10,9 @@ const PORT = process.env.PORT || 5000;
 
 // Use bodyParser
 app.use(bodyParser.json());
+
+// Connect to Database
+connectDB();
 
 // Config for only development
 if (process.env.NODE_ENV === 'development') {
