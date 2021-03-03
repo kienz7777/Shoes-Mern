ef759da7b9a27b482afdf5224de37b542e881b74
kienz7777 <kienvu7777@gmail.com>
2021-03-03 20:41:54 +0700

    add size_product color_product  model

---
+++ server/models/color_product.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+sizeProductSchema = require("../models/size_product").schema;
+
+const Schema = mongoose.Schema;
+const colorProductSchema = new mongoose.Schema({
+    color: {
+        type: Schema.Types.ObjectId,
+        ref: 'Color'
+    },
+    images: {
+        type: Array,
+        default:[]
+    },
+    sizeProducts:[sizeProductSchema],   // Subdocument
+    price: {
+        type: Number,
+        required: true
+    },
+    avatar: {
+        type: Boolean,
+        default: false
+    },
+    product: {
+        type: Schema.Types.ObjectId,
+        ref: 'Product'
+    }
+},{timestamps: true});
+
+module.exports = mongoose.model('ColorProduct',colorProductSchema);
