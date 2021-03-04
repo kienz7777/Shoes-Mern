206d2dce94cd5705159c793413f485331bde86f4
kienz7777 <kienvu7777@gmail.com>
2021-03-04 22:07:42 +0700

    add api get single product server

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -19,3 +19,39 @@ module.exports.addProduct = async (req,res) => {
         console.log(error);
     }
 }
+
+// Get 1 product 
+// api/product/get?id=A
+module.exports.getSingleProduct = (req,res) => {
+
+    Product.find(
+        {_id: req.query.id}
+    ).
+    populate('category').
+    populate({
+        path: 'colorProducts',
+        populate: {
+            path: 'color'
+        }
+    }).
+    populate({ 
+        path: 'colorProducts',
+        populate: {
+          path: 'sizeProducts',
+          populate:{
+              path: 'size'
+          }
+        } 
+    }).
+    populate({ 
+        path: 'review',
+        populate: {
+            path: 'user',
+            select: {'name':1}          // only choose name
+          }
+    }).
+    exec((err,data) => {
+        if (err) return res.send(err);
+        res.status(200).send({data});       // send or json are ok 
+    })
+}
