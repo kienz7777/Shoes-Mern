ea9383901c46f4bbe174d88b62fe92fa7472e395
kienz7777 <kienvu7777@gmail.com>
2021-03-05 20:49:46 +0700

    add get all product api server

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -55,3 +55,65 @@ module.exports.getSingleProduct = (req,res) => {
         res.status(200).send({data});       // send or json are ok 
     })
 }
+
+// Get all products by ARRIVAL
+// api/product/get/?sortBy=createdAt&order=desc&limit=8
+module.exports.getAllProductsByArrival = (req, res) => {
+
+    // let sortBy = req.query.sortBy ? req.query.sortBy : '_id';
+    // let order = req.query.order ? req.query.order : 'asc';
+    let limit = req.body.limit ? parseInt(req.body.limit) : 100
+    let skip = parseInt(req.body.skip);    // vì xài pagination của hook nên mặc định currentPage = 1 nên phải trừ xuống cho skip = 0
+    let findArgs = {};
+
+    //Lấy filters bỏ vào findArgs để get SP theo filters
+    for (let key in req.body.filters){
+        if (req.body.filters[key].length > 0){
+            if (key == "colorProducts.price")  {
+                findArgs[key] = {
+                    // Greater than equal
+                    $gte: req.body.filters[key][0],
+                    // Less than equal
+                    $lte: req.body.filters[key][1]
+                }
+            }       
+            else {
+                findArgs[key] = req.body.filters[key];
+            }
+        }
+    }
+    console.log(findArgs);
+
+    Product.
+    find(findArgs).
+    populate('category').
+    populate({ 
+        path: 'colorProducts',
+        populate: {
+          path: 'color'
+        } 
+    }).
+    populate({ 
+        path: 'review',
+        populate: {
+          path: 'user',
+          select: {'name':1}
+        } 
+    }).
+    populate({ 
+        path: 'colorProducts',
+        populate: {
+          path: 'sizeProducts',
+          populate:{
+              path: 'size'
+          }
+        } 
+    }).
+    // sort({sortBy: order}).
+    limit(limit).
+    skip(skip).
+    exec((err, data)=>{
+        if (err) return res.send(err);
+        res.status(200).send({data});
+    })
+}
