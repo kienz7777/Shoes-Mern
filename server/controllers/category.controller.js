754a31c2b3fa91f4c590d94777a7361540889185
kienz7777 <kienvu7777@gmail.com>
2021-03-05 21:16:21 +0700

    add getall category api server

--- server/controllers/category.controller.js
+++ server/controllers/category.controller.js
@@ -12,3 +12,11 @@ module.exports.addCategory = (req,res) => {
         })
     });
 }
+
+// Get all category -- find(query, projection)
+module.exports.getAllCategories = (req, res) => {
+    Category.find({},(err,categories) => {
+        if (err) return res.send(err);
+        res.status(200).send(categories);
+    })
+}
