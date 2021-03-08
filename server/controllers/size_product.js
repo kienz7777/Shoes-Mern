b4cbd5adb4f5c87ccb767cd36f729fc98cdd7b63
kienz7777 <kienvu7777@gmail.com>
2021-03-08 20:10:22 +0700

    add size_product controller api server

--- server/controllers/size_product.js
+++ server/controllers/size_product.js
@@ -1,11 +1,59 @@
-const express = require('express')
-const router = express.Router()
-
-const sizeProductController = require('../controllers/size_product');
+const SizeProduct = require("../models/size_product");
+const Product = require("../models/product.model");
 
 // Add new size product
-router.post('/add',sizeProductController.addSizeProduct);
+module.exports.addSizeProduct = (req,res) => {
+    const size = new SizeProduct(req.body);
 
-router.put('/update',sizeProductController.updateSizeProduct);
+    size.save((err,doc) => {
+        if (err) return res.json({success: false, err});
+        if (doc){
+            Product.updateOne(
+                {"colorProducts._id": req.body.colorProduct},
+                {$push:{"colorProducts.$.sizeProducts":size}},
+                (err,success) => {
+                    if (err) return res.json({success: false, err});
+                    return res.json({success: true, msg: "New size has been added"});
+                }
+            );
+        }
+    })
 
-module.exports = router;
+}
+
+module.exports.updateSizeProduct = (req,res) => {
+
+    Product.find(
+        {"_id" : req.body.idProduct}
+    ).
+    exec((err, data)=>{
+        {
+            if (data){
+                data.forEach(function(doc){
+                    doc.colorProducts.forEach( function(doc2) {
+                        if (doc2._id == req.body.idColorProduct){
+                            doc2.sizeProducts.forEach(function(doc3){
+                                if (doc3._id == req.body.idSize){
+                                    doc3.quantity = req.body.quantity;
+                                }
+                                else{
+                                    // return res.status(200).json({msg:"Can not find size product"})
+                                }
+                            })
+                        }
+                        else{
+                            // return res.status(200).json({msg:"Can not find color product"})
+                        }
+                        doc.save((err,doc)=>{
+                            if (doc) return res.status(200).json({msg: "Updated!", data:doc})
+                            return res.json({success: false, err})
+                        })
+                    });
+                });
+            
+            }
+            if (err) return res.status(200).json({msg:"Failed"})
+            
+        }
+    })
+}
